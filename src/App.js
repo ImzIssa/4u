e699989e3c968e5a24eb2ce1d59a42b86aac1604
Imran Issa <issaimran94@gmail.com>
2023-01-20 15:51:55 +0200

    working app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import poems from "./data";
+import Poem from "./Poem";
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(() => 45);
+  const [index, setIndex] = useState(() => {
+    return Math.floor(Math.random() * poems.length);
+  });
+  // const { title, author, body, image } = poems[index];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((time) => time - 1);
+      // } else {
+      //   clearInterval(interval);
+      //   setTenzies(true);
+      // }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > poems.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+      setTimeLeft(45);
+    }, 45_000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <div className="title-info">
+            <h2>4YEO 🌹</h2>
+            <span>{timeLeft}</span>
+          </div>
+          <div className="underline"></div>
+        </div>
+        <Poem {...poems[index]} />
+      </section>
+    </main>
   );
 }
 
